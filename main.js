13297ee3f8c02d48ba25a798bac9d649ea0995cb
Puntherline <137522127+TimKoenig96@users.noreply.github.com>
2023-12-24 22:00:54 +0100

    Hands functionality, sticky inputs
    - Added functionality to hands and changed image names for simpler JS functions.
    - Made inputs stick to top once scrolled past to make changing inputs easier.

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 // #region | Variables and elements
+var sticky_inputs;
 var decimal_input_number;
 var decimal_input_slider;
 var simple_output_table;
+var hand_left_e;
+var hand_left;
+var hand_right_e;
+var hand_right;
+
 var old_decimal = 0;
+var decimal_inputs_offset;
 // #endregion
 
 
@@ -14,7 +21,7 @@ function updateAllExamples(decimal) {
 	const binary = decimal.toString(2).padStart(12, "0");
 	const hexadecimal = decimal.toString(16).padStart(3, "0");
 
-	// Adjust table visible parts
+	// Adjust simple table visible parts
 	if (decimal <= 15) {
 		simple_output_table.querySelectorAll("[data-table_visibility]").forEach(function(element) {
 			element.classList.add("table_hidden");
@@ -32,11 +39,36 @@ function updateAllExamples(decimal) {
 		});
 	}
 
-	// Update binary fields
+	// Update simple table fields
 	for (i = 0; i < 12; i++) simple_output_table.rows[1].cells[i + 1].innerText = binary.charAt(i);
-
-	// Update hexadecimal fields
 	for (i = 0; i < 3; i++) simple_output_table.rows[2].cells[i + 1].innerText = hexadecimal.charAt(i);
+
+	// Update hand images
+	var lhand_e, lhand, rhand_e, rhand = "";
+	if (decimal <= 1023) {
+		rhand_e = "/zahlensysteme/media/empty.png";
+		lhand_e = "/zahlensysteme/media/empty.png";
+		rhand = `/zahlensysteme/media/${parseInt(binary.substr(7, 5), 2)}.jpg`;
+		lhand = `/zahlensysteme/media/${parseInt(binary.substr(2, 5).split("").reverse().join(""), 2)}.jpg`;
+	} else if (decimal <= 2047) {
+		var rhand_num = parseInt(binary.substr(7, 5), 2);
+		rhand_e = `/zahlensysteme/media/${rhand_num}_${binary.substr(6, 1)}.png`;
+		lhand_e = `/zahlensysteme/media/empty.png`;
+		rhand = `/zahlensysteme/media/${rhand_num}.jpg`;
+		lhand = `/zahlensysteme/media/${parseInt(binary.substr(1, 5).split("").reverse().join(""), 2)}.jpg`;
+	} else {
+		var rhand_num = parseInt(binary.substr(7, 5), 2);
+		var lhand_num = parseInt(binary.substr(0, 5).split("").reverse().join(""), 2);
+		rhand_e = `/zahlensysteme/media/${rhand_num}_${binary.substr(6, 1)}.png`;
+		lhand_e = `/zahlensysteme/media/${lhand_num}_${binary.substr(5, 1)}.png`;
+		rhand = `/zahlensysteme/media/${rhand_num}.jpg`;
+		lhand = `/zahlensysteme/media/${lhand_num}.jpg`;
+	}
+
+	if (hand_left_e.src !== lhand_e) hand_left_e.src = lhand_e;
+	if (hand_left.src !== lhand) hand_left.src = lhand;
+	if (hand_right_e.src !== rhand_e) hand_right_e.src = rhand_e;
+	if (hand_right.src !== rhand) hand_right.src = rhand;
 }
 
 function updateDecimalInputs() {
@@ -68,9 +100,25 @@ document.addEventListener("DOMContentLoaded", function() {
 	decimal_input_slider = this.getElementById("decimal_input_slider");
 	decimal_input_number = this.getElementById("decimal_input_number");
 	simple_output_table = this.getElementById("simple_output_table");
+	sticky_inputs = this.getElementById("sticky_inputs");
+
+	hand_left_e = this.getElementById("output_hand_left_e");
+	hand_left = this.getElementById("output_hand_left");
+	hand_right_e = this.getElementById("output_hand_right_e");
+	hand_right = this.getElementById("output_hand_right");
 
 	decimal_input_slider.addEventListener("input", updateDecimalInputs);
 	decimal_input_number.addEventListener("input", updateDecimalInputs);
 	this.getElementById("simple_view").addEventListener("change", toggleSimpleView);
+
+	// Make inputs stick once scrolled past
+	decimal_inputs_offset = sticky_inputs.offsetTop;
+	window.addEventListener("scroll", function() {
+		if (window.scrollY > decimal_inputs_offset) {
+			sticky_inputs.style = "top: 0; box-shadow: 0 0 5px black";
+		} else {
+			sticky_inputs.removeAttribute("style");
+		}
+	});
 });
 // #endregion
