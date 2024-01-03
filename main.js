df3e87fe1e170fe55c75fc8ef674424996a39fb2
Puntherline <137522127+TimKoenig96@users.noreply.github.com>
2024-01-03 21:27:30 +0100

    Decimal to binary calculation table
    - Added table showing how to convert decimal to binary.
    - Removed leftover `console.log` from testing.

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ var current_rhand_e;
 var current_rhand = "right_0";
 var current_lhand_e;
 var current_lhand = "left_0";
-
+var to_binary_table;
+var binary_result;
 var old_decimal = 0;
 var decimal_inputs_offset;
 // #endregion
@@ -21,7 +22,7 @@ function updateAllExamples(decimal) {
 	const binary = decimal.toString(2).padStart(12, "0");
 	const hexadecimal = decimal.toString(16).padStart(3, "0");
 
-	// Adjust simple table visible parts
+	// Update simple table visible parts
 	if (decimal <= 15) {
 		simple_output_table.querySelectorAll("[data-table_visibility]").forEach(function(element) {
 			element.classList.add("table_hidden");
@@ -64,13 +65,11 @@ function updateAllExamples(decimal) {
 
 	// Check if they need to be updated
 	if (rhand !== current_rhand) {
-		console.log(`Update right hand from ${current_rhand} to ${rhand}`);
 		document.getElementById(rhand).classList.remove("hand_hidden");
 		document.getElementById(current_rhand).classList.add("hand_hidden");
 		current_rhand = rhand;
 	}
 	if (rhand_e !== current_rhand_e) {
-		console.log(`Update right hand E from ${current_rhand_e} to ${rhand_e}`);
 		if (current_rhand_e === undefined) {
 			document.getElementById(rhand_e).classList.remove("hand_hidden");
 		} else {
@@ -84,13 +83,11 @@ function updateAllExamples(decimal) {
 		current_rhand_e = rhand_e;
 	}
 	if (lhand !== current_lhand) {
-		console.log(`Update left hand from ${current_lhand} to ${lhand}`);
 		document.getElementById(lhand).classList.remove("hand_hidden");
 		document.getElementById(current_lhand).classList.add("hand_hidden");
 		current_lhand = lhand;
 	}
 	if (lhand_e !== current_lhand_e) {
-		console.log(`Update right hand E from ${current_lhand_e} to ${lhand_e}`);
 		if (current_lhand_e === undefined) {
 			document.getElementById(lhand_e).classList.remove("hand_hidden");
 		} else {
@@ -102,6 +99,47 @@ function updateAllExamples(decimal) {
 			}
 		}
 		current_lhand_e = lhand_e;
+	}
+
+	// Update decimal to binary calculation table visible parts and binary result
+	if (decimal <= 15) {
+		to_binary_table.querySelectorAll("[data-table_visibility]").forEach(function(element) {
+			element.classList.add("table_hidden");
+		});
+		to_binary_table.rows[0].cells[1].setAttribute("rowspan", "5");
+		binary_result.innerText = binary.slice(8);
+	} else if (decimal <= 255) {
+		to_binary_table.querySelectorAll("[data-table_visibility='2']").forEach(function(element) {
+			element.classList.add("table_hidden");
+		});
+		to_binary_table.querySelectorAll("[data-table_visibility='1']").forEach(function(element) {
+			element.classList.remove("table_hidden");
+		});
+		to_binary_table.rows[0].cells[1].setAttribute("rowspan", "9");
+		binary_result.innerText = binary.slice(4).match(/.{1,4}/g).join(" ");
+	} else {
+		to_binary_table.querySelectorAll("[data-table_visibility]").forEach(function(element) {
+			element.classList.remove("table_hidden");
+		});
+		to_binary_table.rows[0].cells[1].setAttribute("rowspan", "13");
+		binary_result.innerText = binary.match(/.{1,4}/g).join(" ");
+	}
+
+	// Update decimal to binary calculation table fields
+	var remaining_decimal = decimal;
+	for (i = 0; i < 12; i++) {
+
+		// Update decimal field
+		to_binary_table.rows[i + 1].cells[0].innerText = remaining_decimal;
+
+		// Update rest field
+		to_binary_table.rows[i + 1].cells[2].innerHTML = (remaining_decimal % 2 == 1 ? "Ja (1)" : "Nein (0)");
+
+		// Update remaining decimal number
+		remaining_decimal = Math.floor(remaining_decimal / 2);
+
+		// Update result field
+		to_binary_table.rows[i + 1].cells[1].innerText = remaining_decimal;
 	}
 }
 
@@ -135,6 +173,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	decimal_input_number = this.getElementById("decimal_input_number");
 	simple_output_table = this.getElementById("simple_output_table");
 	sticky_inputs = this.getElementById("sticky_inputs");
+	to_binary_table = this.getElementById("to_binary_table");
+	binary_result = this.getElementById("binary_result");
 
 	decimal_input_slider.addEventListener("input", updateDecimalInputs);
 	decimal_input_number.addEventListener("input", updateDecimalInputs);
