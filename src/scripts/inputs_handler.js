583d9237464c4742f6c67061ef630757038aa57a
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2025-03-12 21:51:52 +0100

    add: Get required section of binary string on input update

--- src/scripts/inputs_handler.js
+++ src/scripts/inputs_handler.js
@@ -1,14 +1,45 @@
 let numberInput;
 let rangeInput;
 
+let inputValue = 0;
+let timeout;
+
+function updateHandsCanvas() {
+
+	// Convert input to binary
+	const binary = inputValue.toString(2).padStart(12, "0");
+
+	// Extract required binary sections
+	let rightHandBinary, leftHandBinary, leftExtra, rightExtra;
+
+	rightHandBinary = binary.slice(-5);
+
+	if (inputValue < 1024) {
+		leftHandBinary = binary.slice(2, 7);
+	} else if (inputValue < 2048) {
+		leftHandBinary = binary.slice(1, 6);
+		rightExtra = binary[6];
+	} else {
+		leftHandBinary = binary.slice(0, 5);
+		leftExtra = binary[5];
+		rightExtra = binary[6];
+	}
+}
+
 function decInputHandler(event) {
 
 	// Get value
-	let value = event.target.value;
+	inputValue = parseInt(event.target.value);
 
 	// Set the other input to the same value
-	if (event.target.type === "number") rangeInput.value = value;
-	else numberInput.value = value;
+	if (event.target.type === "number") rangeInput.value = inputValue;
+	else numberInput.value = inputValue;
+
+	// Clear timeout
+	clearTimeout(timeout);
+
+	// Set timeout
+	timeout = setTimeout(updateHandsCanvas, 250);
 }
 
 export function setupInputHandlers() {
