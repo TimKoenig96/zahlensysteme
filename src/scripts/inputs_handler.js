e0643e7923dcc410dae88d20cbba5899c3294263
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2025-03-13 16:59:31 +0100

    add: Functions for getting hand/finger info

--- src/scripts/inputs_handler.js
+++ src/scripts/inputs_handler.js
@@ -4,6 +4,54 @@ let rangeInput;
 let inputValue = 0;
 let timeout;
 
+let spritesheetImg, fingerRetractedImg, fingerExtendedImg;
+
+/**
+ * Get hand indexes on spritesheet.
+ * @param {String} binary Input as binary string
+ * @returns {Object} Left and right hand indexes
+ */
+function getHandsSpritesheetIndexes(binary) {
+	let leftHandIndex;
+
+	// Right hand is always the last 5 characters
+	let rightHandIndex = parseInt(binary.slice(-5), 2);
+
+	// No extra fingers required: Left hand indexes 2 to 7
+	if (inputValue < 1024) leftHandIndex = parseInt(binary.slice(2, 7));
+
+	// One extra finger required: Left hand indexes 1 to 6
+	else if (inputValue < 2048) leftHandIndex = parseInt(binary.slice(1, 6));
+
+	// Both extra fingers required: Left hand indexes 0 to 5
+	else leftHandIndex = parseInt(binary.slice(0, 5));
+
+	return { leftHandIndex, rightHandIndex };
+}
+
+/**
+ * Get optional fingers statuses. -1 for hidden, 0 for retracted, 1 for extended.
+ * @param {String} binary Input as binary string
+ * @returns {Object} Left and right finger statuses
+ */
+function getOptionalFingersStatuses(binary) {
+
+	// Hidden by default
+	let leftFingerStatus = -1;
+	let rightFingerStatus = -1;
+
+	// Two extra fingers required
+	if (inputValue >= 2048) {
+		leftFingerStatus = parseInt(binary[5], 2);
+		rightFingerStatus = parseInt(binary[6], 2);
+	}
+
+	// One extra finger required
+	else if (inputValue >= 1024) rightFingerStatus = parseInt(binary[6], 2);
+
+	return { leftFingerStatus, rightFingerStatus };
+}
+
 function updateHandsCanvas() {
 
 	// Convert input to binary
